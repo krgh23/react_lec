590962162e587d6ca89b18d3c5812c6dbeb15d86
krgh23 <krgh23@naver.com>
2025-02-26 17:41:42 +0900

    20250226

---
+++ 21_crud_app/src/App.js
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import BasicLayout from './layout/BasicLayout';
+import IndexPage from './pages/IndexPage';
+import ListPage from './pages/blog/ListPage';
+import RegistPage from './pages/blog/RegistPage';
+import DetailPage from './pages/blog/DetailPage';
+import EditPage from './pages/blog/EditPage';
+
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<BasicLayout/>}>
+            <Route path='/'                element={<IndexPage/>}></Route>
+            <Route path='/blog/List'       element={<ListPage/>}></Route>
+            <Route path='/blog/regist'     element={<RegistPage/>}></Route>
+            <Route path='/blog/detail/:id' element={<DetailPage/>}></Route>
+            <Route path='/blog/edit/:id'   element={<EditPage/>}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
